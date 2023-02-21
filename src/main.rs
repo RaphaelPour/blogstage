d82fc59381dbf012d069662e7a61b253bf57cb8e
Raphael Pour <info@raphaelpour.de>
2023-02-21 22:31:49 +0100

    main: list files

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,53 @@
+use std::fs;
+use std::io;
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
 
 // https://doc.rust-lang.org/book/ch20-01-single-threaded.html
 
 fn main(){
-    let listener = match TcpListener::bind("127.0.0.1:8080") {
+    /* parse arguments */
+    let uri = match std::env::args().nth(1){
+        Some(uri) => uri,
+        None => {
+            println!("usage: blogstage <URI> <PATH>");
+            return
+        }
+    };
+
+    let path = match std::env::args().nth(2) {
+        Some(path) => path,
+        None => {
+            println!("usage: blogstage <URI> <PATH>");
+            return
+        }
+    };
+
+    /* load files */
+    let raw_entries = match fs::read_dir(path.clone()) {
+        Ok(entries) => entries,
+        Err(e) => {
+            println!("error reading files from {}: {}", path, e);
+            return
+        }
+    };
+
+    let entries = raw_entries.filter(|entry| entry.as_ref().unwrap().path().is_file())
+        .map(|res| res.map(|e| e.file_name()))
+        .collect::<Result<Vec<_>, io::Error>>().unwrap();
+
+    /* start server */
+    let listener = match TcpListener::bind(uri.clone()) {
         Ok(l) => l,
         Err(e) => {
-            println!("error on binding to 127.0.0.1:8080: {}", e);
+            println!("error on binding to {}: {}", uri, e);
             return
         }
     };
 
     for stream in listener.incoming() {
         match stream {
-            Ok(s) => on_request(s),
+            Ok(s) => on_request(s, entries.clone(), path.clone()),
             Err(e) => {
                 println!("error accepting connection: {}", e);
                 continue
@@ -23,13 +56,16 @@ fn main(){
     }
 }
 
-fn on_request(mut stream: TcpStream) {
+fn on_request(mut stream: TcpStream, files: Vec<std::ffi::OsString>, path: String) {
     let reader = BufReader::new(&mut stream);
     let request: Vec<_> = reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
+
+    println!("{:?}", request);
+    println!("{}: {:?}", path, files);
 
     let response = "HTTP/1.1 200 OK\r\n\r\nblogstage\r\n";
     stream.write_all(response.as_bytes()).unwrap();

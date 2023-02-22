46d052694ccd013796078457433d7201473f0e87
Raphael Pour <info@raphaelpour.de>
2023-02-22 12:41:54 +0100

    main: apply rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use std::fs;
-use std::path::PathBuf;
 use std::collections::HashMap;
+use std::fs;
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
+use std::path::PathBuf;
 
 // https://doc.rust-lang.org/book/ch20-01-single-threaded.html
 
-fn main(){
+fn main() {
     /* parse arguments */
-    let uri = match std::env::args().nth(1){
+    let uri = match std::env::args().nth(1) {
         Some(uri) => uri,
         None => {
             println!("usage: blogstage <URI> <PATH>");
-            return
+            return;
         }
     };
 
@@ -20,7 +20,7 @@ fn main(){
         Some(path) => path,
         None => {
             println!("usage: blogstage <URI> <PATH>");
-            return
+            return;
         }
     };
 
@@ -29,22 +29,27 @@ fn main(){
         Ok(entries) => entries,
         Err(e) => {
             println!("error reading files from {}: {}", path, e);
-            return
+            return;
         }
     };
 
     let mut files = HashMap::new();
 
-
     for entry in raw_entries {
-        if !entry.as_ref().unwrap().path().is_file(){
+        if !entry.as_ref().unwrap().path().is_file() {
             continue;
         }
 
         files.insert(
-                entry.as_ref().unwrap().file_name().into_string().unwrap().clone(),
-                entry.as_ref().unwrap().path().clone(),
-            );
+            entry
+                .as_ref()
+                .unwrap()
+                .file_name()
+                .into_string()
+                .unwrap()
+                .clone(),
+            entry.as_ref().unwrap().path().clone(),
+        );
     }
 
     /* start server */
@@ -52,7 +57,7 @@ fn main(){
         Ok(l) => l,
         Err(e) => {
             println!("error on binding to {}: {}", uri, e);
-            return
+            return;
         }
     };
 
@@ -61,7 +66,7 @@ fn main(){
             Ok(s) => on_request(s, files.clone()),
             Err(e) => {
                 println!("error accepting connection: {}", e);
-                continue
+                continue;
             }
         }
     }
@@ -75,7 +80,7 @@ fn on_request(mut stream: TcpStream, files: HashMap<String, PathBuf>) {
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let mut target:String = request[0].split(' ').collect::<Vec<&str>>()[1][1..].to_string();
+    let mut target: String = request[0].split(' ').collect::<Vec<&str>>()[1][1..].to_string();
 
     if target == "" {
         target = "index.html".into()
@@ -88,13 +93,12 @@ fn on_request(mut stream: TcpStream, files: HashMap<String, PathBuf>) {
 
             println!("200 {}", target);
             format!("HTTP/1.1 200 OK\r\nContent-Length: {length}\r\n\r\n{body}")
-        },
+        }
         None => {
             println!("404 {}", target);
             "HTTP/1.1 404 NOT FOUND\r\n\r\n".into()
         }
     };
-
 
     stream.write_all(response.as_bytes()).unwrap();
 }

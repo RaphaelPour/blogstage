a6e92235dcb9200e97fca926b63c2b5e2ca6ac00
Raphael Pour <info@raphaelpour.de>
2023-02-22 12:41:00 +0100

    main: server files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
-use std::io;
+use std::path::PathBuf;
+use std::collections::HashMap;
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
 
@@ -32,9 +33,19 @@ fn main(){
         }
     };
 
-    let entries = raw_entries.filter(|entry| entry.as_ref().unwrap().path().is_file())
-        .map(|res| res.map(|e| e.file_name()))
-        .collect::<Result<Vec<_>, io::Error>>().unwrap();
+    let mut files = HashMap::new();
+
+
+    for entry in raw_entries {
+        if !entry.as_ref().unwrap().path().is_file(){
+            continue;
+        }
+
+        files.insert(
+                entry.as_ref().unwrap().file_name().into_string().unwrap().clone(),
+                entry.as_ref().unwrap().path().clone(),
+            );
+    }
 
     /* start server */
     let listener = match TcpListener::bind(uri.clone()) {
@@ -47,7 +58,7 @@ fn main(){
 
     for stream in listener.incoming() {
         match stream {
-            Ok(s) => on_request(s, entries.clone(), path.clone()),
+            Ok(s) => on_request(s, files.clone()),
             Err(e) => {
                 println!("error accepting connection: {}", e);
                 continue
@@ -56,7 +67,7 @@ fn main(){
     }
 }
 
-fn on_request(mut stream: TcpStream, files: Vec<std::ffi::OsString>, path: String) {
+fn on_request(mut stream: TcpStream, files: HashMap<String, PathBuf>) {
     let reader = BufReader::new(&mut stream);
     let request: Vec<_> = reader
         .lines()
@@ -64,9 +75,26 @@ fn on_request(mut stream: TcpStream, files: Vec<std::ffi::OsString>, path: Strin
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("{:?}", request);
-    println!("{}: {:?}", path, files);
+    let mut target:String = request[0].split(' ').collect::<Vec<&str>>()[1][1..].to_string();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\nblogstage\r\n";
+    if target == "" {
+        target = "index.html".into()
+    }
+
+    let response = match files.get(&target) {
+        Some(path) => {
+            let body = fs::read_to_string(path).unwrap();
+            let length = body.len();
+
+            println!("200 {}", target);
+            format!("HTTP/1.1 200 OK\r\nContent-Length: {length}\r\n\r\n{body}")
+        },
+        None => {
+            println!("404 {}", target);
+            "HTTP/1.1 404 NOT FOUND\r\n\r\n".into()
+        }
+    };
+
+
     stream.write_all(response.as_bytes()).unwrap();
 }
